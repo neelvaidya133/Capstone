4182bff8a32abc5fe68e4787287145ce57570245
bhojani-deep <deep.180670107008@gmail.com>
2024-07-18 20:50:51 -0400

    Login form validation

--- src/Pages/LoginPage/LoginPage.js
+++ src/Pages/LoginPage/LoginPage.js
@@ -1,9 +1,49 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import "./LoginPage.css";
 import loginImage from "../../Assets/login.svg";
 
 const LoginPage = () => {
+  // Using the useState hook, creating a state variables for email, password & errors along with their setter functions and setting their initial values
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState({ email: "", password: "" });
+  // With the help of useNavigate() hook, we can easily navigate to different pages
+  const navigate = useNavigate();
+
+  // Function for the validation of the login form
+  const validateForm = () => {
+    let loginFormErrors = {};
+    let isValid = true;
+
+    // Email Address validation
+    if (!email) {
+      isValid = false;
+      loginFormErrors["email"] = "Please enter an Email Address";
+    } else if (!/\S+@\S+\.\S+/.test(email)) {
+      isValid = false;
+      loginFormErrors["email"] = "Email address is not valid";
+    }
+
+    // Password validation
+    if (!password) {
+      isValid = false;
+      loginFormErrors["password"] = "Please enter a Password";
+    }
+
+    setErrors(loginFormErrors);
+    return isValid;
+  };
+
+  // Function to handle login form submission
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // If form is valid, then redirect to the Dashboard page
+    if (validateForm()) {
+      navigate("/dashboard");
+    }
+  };
+
   return (
     <div className="login-content">
       <div class="login-left-part">
@@ -12,15 +52,18 @@ const LoginPage = () => {
       <div class="login-right-part">
         <h1>Welcome to Tailor's Data</h1>
         <h2>Log In</h2>
-        <form>
+        <form onSubmit={handleSubmit}>
           <label for="email">Email</label>
           <input
             type="email"
             id="email"
             name="email"
             placeholder="Enter your email address"
-            required
+            value={email} 
+            onChange={(e) => setEmail(e.target.value)} // Updating the email state with the help of onChange
           />
+          {/* If error exists for the email address, then it will be displayed */}
+          {errors.email && <p className="login-error-msg">{errors.email}</p>}
 
           <label for="password">Password</label>
           <input
@@ -28,8 +71,11 @@ const LoginPage = () => {
             id="password"
             name="password"
             placeholder="Enter your password"
-            required
+            value={password}
+            onChange={(e) => setPassword(e.target.value)} // Updating the password state with the help of onChange
           />
+          {/* If error exists for the password, then it will be displayed */}
+          {errors.password && <p className="login-error-msg">{errors.password}</p>}
 
           <button type="submit">Login</button>
         </form>
