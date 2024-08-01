9b09b22b69e10eb8ec9aafe1d5065b1f9f36212b
bhojani-deep <deep.180670107008@gmail.com>
2024-08-01 17:42:14 -0400

    try to merge main into deep

--- src/graphql/Mutation/CreateOrder.js
+++
@@ -1,9 +0,0 @@
-import { gql } from "@apollo/client";
-
-export const CREATE_ORDER = gql`
-  mutation createOrder($order: OrderInput!) {
-    createOrder(input: { order: $order }) {
-      clientMutationId
-    }
-  }
-`;
--- src/graphql/Mutation/Signin.js
+++
@@ -1,9 +0,0 @@
-import { gql } from "@apollo/client";
-
-export const SIGNIN = gql`
-  mutation Signin($email: String!, $password: String!) {
-    signin(input: { email: $email, password: $password }) {
-      jwtToken
-    }
-  }
-`;
